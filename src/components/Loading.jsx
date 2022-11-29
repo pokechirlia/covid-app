b137495c7975dd9b38f4dadd97257662e6376569
pokechirlia <chi.k.tran2000@gmail.com>
2022-11-29 12:30:37 +1100

    initial commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//sfc to create component
+
+const Loading = () => {
+  return (
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  );
+};
+
+export default Loading;
