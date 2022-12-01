970232e2fafe3d5ce97efcc3a30f68c2af4327f3
pokechirlia <chi.k.tran2000@gmail.com>
2022-12-01 20:23:29 +1100

    pie chart is added

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,23 +2,34 @@ import React, { Component } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 class Chart extends Component {
   render() {
     const { countries } = this.props;
 
-    //if (countries.length === 0) return <div></div>;
+    if (countries.length === 0) return <div></div>;
 
     const data = {
       labels: countries.map((c) => c.name),
       datasets: [
         {
           data: countries.map((c) => c.total),
-          //   backgroundColor: "red",
+          backgroundColor: ["orange", "blue", "red", "purple", "green"],
         },
       ],
     };
 
-    return <div>Selected {countries.length} countries</div>;
+    return (
+      <div>
+        <Pie
+          data={data}
+          width={100}
+          height={300}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+    );
   }
 }
 
